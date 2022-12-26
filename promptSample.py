852cf2ec2e282daba89a465b863ad4db336da420
Reuben Odunze <royalrubee@gmail.com>
2022-12-26 08:56:05 +0100

    Create promptSample.py

---
+++ promptSample.py
@@ -0,0 +1,14 @@
+  prompts = [
+        {
+            "hashtag": "#NFT",
+            "text": "post a meme about NFT"
+        },
+        {
+            "hashtag": "#AI",
+            "text": "tweet something cool for #stabledifussion"
+        },
+        {
+            "hashtag": "@royaliben",
+            "text": "tweet something cool for @royaliben from twitter"
+        },
+    ]
