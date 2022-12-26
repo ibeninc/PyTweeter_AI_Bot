e9e7795861b4182a57a8524032ada8a7f07661c6
Reuben Odunze <royalrubee@gmail.com>
2022-12-26 08:44:27 +0100

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,6 @@
+twitter_key="YOUR API KEY FROM TWITTER"
+twitter_secret ="YOUR API KEY SECRET FROM TWITTER"
+twitter_access_token="YOUR ACCESS TOKEN FROM TWITTER"
+twitter_access_token_secret="YOUR ACCESS TOKEN SECRET FROM TWITTER"
+
+openai_key = "OPEN AI KEY"
